ccc89fe26ce50b7b6427f89e95037926586af5fe
emdzej <jurcak.matej@gmail.com>
2024-05-27 14:36:00 +0200

    Jurgen images, sliders, perf improvements

---
+++ process_images.sh
@@ -0,0 +1,17 @@
+# Convert .jpg files to .webp if the .webp file does not already exist
+for file in images/*.jpg; do
+    webp_file="${file%.*}.webp"
+    if [ ! -f "$webp_file" ]; then
+        echo "Converting $file to $webp_file..."
+        cwebp "$file" -o "$webp_file"
+    fi
+done
+
+# Resize images to 35%, creating a _small version if it doesn't already exist
+for file in images/*.{jpg,webp}; do
+    if [[ "$file" != *_small.jpg && "$file" != *_small.webp && ! -f "${file%.*}_small.${file##*.}" ]]; then
+        echo "Converting $file to small version..."
+        magick "$file" -resize 35% "${file%.*}_small.${file##*.}"
+    fi
+done
+
