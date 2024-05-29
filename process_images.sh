f12b902db7072496af3f184908ede1accd0fd77b
emdzej <jurcak.matej@gmail.com>
2024-05-29 23:03:07 +0200

    another one of jpg/webp minifying and tidying

--- process_images.sh
+++ process_images.sh
@@ -1,17 +1,17 @@
 # Convert .jpg files to .webp if the .webp file does not already exist
-for file in images/*.jpg; do
+for file in images/large/*.jpg; do
     webp_file="${file%.*}.webp"
     if [ ! -f "$webp_file" ]; then
         echo "Converting $file to $webp_file..."
-        cwebp "$file" -q 50 -o "$webp_file"
+        cwebp "$file" -q 40 -o "$webp_file"
     fi
 done
 
-# Resize images to 35%, creating a _small version if it doesn't already exist
-for file in images/*.webp; do
-    if [[ "$file" != *_small.webp && ! -f "${file%.*}_small.webp" ]]; then
+# Resize images to 35%, overwriting if the small version already exists
+for file in images/large/*.{jpg,webp}; do
+    if [[ ! -f "images/small/$(basename "$file")" ]]; then
         echo "Converting $file to small version..."
-        magick "$file" -resize 35% "${file%.*}_small.webp"
+        magick "$file" -resize 35% "images/small/$(basename "$file")"
     fi
 done
 
