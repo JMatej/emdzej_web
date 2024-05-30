d8a478ef64b04dc7cd6eaa226475d74a8102f9b9
emdzej <jurcak.matej@gmail.com>
2024-05-30 19:07:33 +0200

    remove picture, use srcset and sizes

--- process_images.sh
+++ process_images.sh
@@ -1,17 +1,26 @@
 # Convert .jpg files to .webp if the .webp file does not already exist
-for file in images/large/*.jpg; do
-    webp_file="${file%.*}.webp"
-    if [ ! -f "$webp_file" ]; then
-        echo "Converting $file to $webp_file..."
-        cwebp "$file" -q 40 -o "$webp_file"
-    fi
-done
+#for file in images/minified/*.jpg; do
+#   webp_file="${file%.*}.webp"
+#    if [ ! -f "$webp_file" ]; then
+#        echo "Converting $file to $webp_file..."
+#        cwebp "$file" -q 40 -o "$webp_file"
+#    fi
+#done
 
-# Resize images to 35%, overwriting if the small version already exists
-for file in images/large/*.{jpg,webp}; do
-    if [[ ! -f "images/small/$(basename "$file")" ]]; then
-        echo "Converting $file to small version..."
-        magick "$file" -resize 35% "images/small/$(basename "$file")"
-    fi
+# Resize images to specified widths, creating new versions if they don't exist
+for file in images/minified/*.{jpg,webp}; do
+    base=$(basename "$file")
+    filename="${base%.*}"
+    extension="${base##*.}"
+
+    declare -a widths=(300 600 820 1160 1420 1640)
+
+    for width in "${widths[@]}"; do
+        output="images/public/${filename}${width}w.${extension}"
+        if [[ ! -f "$output" ]]; then
+            echo "Converting $file to $width width version..."
+            magick "$file" -resize "${width}x" "$output"
+        fi
+    done
 done
 
